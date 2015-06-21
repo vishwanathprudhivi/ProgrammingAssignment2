15def1f06f867a5133c608a8ee4ca327be255e04
vishwanathprudhivi <vishwanath.prudhivi@gmail.com>
2015-06-21 13:37:16 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,5 @@
+# This function creates an invertible matrix. Also, within the same matrix the user has the liberty to update the matrix as well 
+# as update the inverse with "set_matrix" and "set_inverse". The user can display these values using "get_matrix" and "get_inverse"
 makeCacheMatrix <- function( x = matrix())
 {    		
 		original_matrix <<- x
@@ -21,7 +23,8 @@ makeCacheMatrix <- function( x = matrix())
 		}		
 		list( set_matrix = set_matrix, get_matrix = get_matrix, set_inverse = set_inverse, get_inverse = get_inverse)
 }
-
+#This function solves and displays the inverse of the matrix. If the inverse already exists using "set_inverse" then the same 
+# value will be pulled from the cache and displayed. If it doesn't exist then a new inverse will be created and displayed.
 
 cacheSolve <- function(x,...)
 {
